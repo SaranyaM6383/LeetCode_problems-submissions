d7088f818d850726502fc21c1658b128c3f7e679
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-02 05:49:21 +0100

    Add solution for 205. Isomorphic Strings

---
+++ easy/205-isomorphic-strings/205-isomorphic-strings.c
@@ -0,0 +1,14 @@
+            }
+            if (!charIndexS.get(s.charAt(i)).equals(charIndexT.get(t.charAt(i)))) {
+            return false;
+            }
+                charIndexT.put(t.charAt(i),i);
+            return true;
+        }
+            if(!charIndexT.containsKey(t.charAt(i))){
+            }
+            if(!charIndexS.containsKey(s.charAt(i))){
+                charIndexS.put(s.charAt(i),i);
+    
+            }
+}
