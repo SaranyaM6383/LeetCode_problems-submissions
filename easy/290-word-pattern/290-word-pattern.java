a95cff4887cd04276386b229694f16456a149303
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-02 06:18:54 +0100

    Add solution for 290. Word Pattern

---
+++ easy/290-word-pattern/290-word-pattern.java
@@ -0,0 +1,11 @@
+                if (seenWords.contains(w)) 
+                    return false;}
+                charToWord.put(c, w);
+                seenWords.add(w);
+                {
+                }
+            }
+        return true;
+    }
+}
+                
