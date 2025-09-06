a82e94ffb7974f2c49c20eeb3c343bdd2d208a12
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-06 10:08:54 +0100

    Create 1446_Consecutive_Characters.java

---
+++ 1446_Consecutive_Characters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxPower(String s) {
+        int n = s.length();
+        int max = 0;
+        int count = 1;
+
+        if (n == 1) {
+            return 1;
+        }
+
+        for (int i = 1; i < n; i++) {
+            if (s.charAt(i) == s.charAt(i - 1)) {
+                count++;
+            } else {
+                count = 1;
+            }
+            max = Math.max(max, count);
+        }
+        return max;
+    }
+}
+
+
+                           
