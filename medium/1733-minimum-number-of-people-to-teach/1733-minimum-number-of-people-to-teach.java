38a2271710294787d8b38d0c2afc4de9c9aaba94
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-10 15:22:23 +0100

    Add solution for 1733. Minimum Number of People to Teach

---
+++ medium/1733-minimum-number-of-people-to-teach/1733-minimum-number-of-people-to-teach.java
@@ -0,0 +1,14 @@
+            int u2 = friendship[1] - 1;
+            boolean canCommunicate = false;
+            
+            for (int lang1 : userLanguages[u1]) {
+                for (int lang2 : userLanguages[u2]) {
+        
+     
+        for (int[] friendship : friendships) {
+            int u1 = friendship[0] - 1;
+class Solution {
+    public int minimumTeachings(int totalLanguages, int[][] userLanguages, int[][] 
+friendships) {
+        Set<Integer> usersToTeach = new HashSet<>();
+import java.util.*;
