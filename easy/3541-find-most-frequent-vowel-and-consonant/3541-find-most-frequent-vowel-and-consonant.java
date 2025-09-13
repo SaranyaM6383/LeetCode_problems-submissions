5fd5c9258f789daf1721720b1a9fecaa39c49149
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-13 03:50:19 +0100

    Add solution for 3541. Find Most Frequent Vowel and Consonant

---
+++ easy/3541-find-most-frequent-vowel-and-consonant/3541-find-most-frequent-vowel-and-consonant.java
@@ -0,0 +1,13 @@
+            maxConso=Math.max(maxConso,freq[i]);
+        }
+        return maxVowel+maxConso;
+    }
+}
+            
+            else
+            freq[i]++;
+            int i=c-'a';
+        for(char c: s.toCharArray()){
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+             maxVowel = Math.max(maxVowel, freq[i]);
+        
