c3f686f723b6940fa5d25731ecbb4bd4f6313d30
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-14 04:24:22 +0100

    Add solution for 455. Assign Cookies

---
+++ easy/455-assign-cookies/455-assign-cookies.java
@@ -0,0 +1,14 @@
+                childIndex--;
+                cookieIndex--;
+                maxNum++;
+            if(s[cookieIndex] >= g[childIndex]){
+        while(cookieIndex >= 0 && childIndex >=0){
+        int childIndex = g.length-1;
+        int cookieIndex = cookiesNums-1;
+        int maxNum=0;
+        Arrays.sort(g);
+        Arrays.sort(s);
+        if(cookiesNums == 0) return 0;
+        int cookiesNums = s.length;
+    public int findContentChildren(int[] g, int[] s) {
+class Solution {
