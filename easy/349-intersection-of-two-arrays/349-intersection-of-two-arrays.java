157d1e3d519df6ff8ef7e882a4703e5e02360656
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-15 05:23:15 +0100

    Add solution for 349. Intersection of Two Arrays

---
+++ easy/349-intersection-of-two-arrays/349-intersection-of-two-arrays.java
@@ -0,0 +1,16 @@
+            }
+            if(nums2set.contains(num)){
+                list1.add(num);
+        for(int num : nums1set){
+        }
+        int[] arr = new int[list1.size()];
+        int i = 0;
+        for(int num : list1){
+            arr[i] = num;
+            i++;
+        }
+        }
+            nums2set.add(num);
+        for(int num : nums2){
+        }
+            nums1set.add(num);
