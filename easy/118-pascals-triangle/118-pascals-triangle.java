3276a7d7d341776c47c858dd3eb1f3e829b70692
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-22 14:30:50 +0100

    Add solution for 118. Pascal's Triangle

---
+++ easy/118-pascals-triangle/118-pascals-triangle.java
@@ -0,0 +1,12 @@
+                row.add(prevRow.get(j - 1) + prevRow.get(j));
+            }
+            row.add(1);
+            for (int j = 1; j < rowNum; j++) {
+            List<Integer> prevRow = triangle.get(rowNum - 1);
+            List<Integer> row = new ArrayList<>();
+        for (int rowNum = 1; rowNum < numRows; rowNum++) {
+        triangle.get(0).add(1);
+            row.add(1);
+            triangle.add(row);
+        }
+        return triangle;
