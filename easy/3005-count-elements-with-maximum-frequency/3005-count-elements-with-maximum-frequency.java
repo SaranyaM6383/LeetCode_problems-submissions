f40f4797c5120bd248523ec18d679b2a3e98f6e1
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-22 14:27:32 +0100

    Add solution for 3005. Count Elements With Maximum Frequency

---
+++ easy/3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.java
@@ -0,0 +1,13 @@
+            byte f = ++freq[n];
+            if (f > max){
+            max = f;
+            res = f;
+        }
+    }
+        else if(f == max)
+        res+=f;
+    }
+    return res;
+        for(int n : nums) {
+        byte max = 0, res = 0;
+}
