e52f8c1a496cea805bcf39fe939c41377abc2174
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-25 15:56:34 +0100

    Add solution for 2749. Minimum Operations to Make the Integer Zero

---
+++ medium/2749-minimum-operations-to-make-the-integer-zero/2749-minimum-operations-to-make-the-integer-zero.java
@@ -0,0 +1,15 @@
+           
+            }
+            if(k>=Long.bitCount(x)){
+                return k;
+            }
+        }
+        return -1;
+    }
+}
+            if(x<k){
+                return -1;
+    public int makeTheIntegerZero(int num1, int num2) {
+        for(int k=1;k<=60;k++){
+            long x=1L * num1 - 1L * num2 * k;
+class Solution {
