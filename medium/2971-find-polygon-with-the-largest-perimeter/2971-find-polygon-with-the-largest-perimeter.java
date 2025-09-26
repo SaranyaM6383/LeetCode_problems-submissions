0adb26ee2ca2d0b56d6db60aa12bccacd0c8f272
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-26 17:15:39 +0100

    Add solution for 2971. Find Polygon With the Largest Perimeter

---
+++ medium/2971-find-polygon-with-the-largest-perimeter/2971-find-polygon-with-the-largest-perimeter.java
@@ -0,0 +1,12 @@
+            sum+=num;
+        }
+        int n=nums.length;
+        for(int i=n-1;i>=2;i--){
+            sum-=nums[i];
+        }
+            if(sum>nums[i]){
+                return sum+nums[i];
+            }
+    }
+        return -1;
+}
