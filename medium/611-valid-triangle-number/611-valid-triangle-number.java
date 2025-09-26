8d1e187f8a51570aa16bd6e5463121c910117e69
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-09-26 17:09:59 +0100

    Add solution for 611. Valid Triangle Number

---
+++ medium/611-valid-triangle-number/611-valid-triangle-number.java
@@ -0,0 +1,16 @@
+                    left++;
+                }
+            }
+        }
+        return count;
+                    right--;
+                }
+                else{
+                    count+=right-left;
+                if(nums[left]+nums[right]>nums[i]){
+            while(left<right){
+            int left=0,right=i-1;
+        for(int i=n-1;i>=2;i--){
+        int count=0;
+        int n=nums.length;
+        Arrays.sort(nums);
