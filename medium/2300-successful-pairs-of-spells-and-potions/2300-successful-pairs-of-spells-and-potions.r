2ecbba836384006583e80f93474938119ac84193
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-10-08 17:28:56 +0100

    Add solution for 2300. Successful Pairs of Spells and Potions

---
+++ medium/2300-successful-pairs-of-spells-and-potions/2300-successful-pairs-of-spells-and-potions.r
@@ -0,0 +1,15 @@
+            int mid=low+(high-low)/2;
+        while(low<=high){
+        int low=0, high = potions.length-1,idx=-1;
+    }
+    int bs(int potions[],long strength,long success){
+        }
+        return ans;
+            if(idx!=-1) ans[i]=potions.length-idx;
+            int idx=bs(potions, spells[i],success);
+        for(int i=0;i<n;i++){
+        int ans[] =new int[n];
+        Arrays.sort(potions);
+        int n=spells.length;
+    public int[] successfulPairs(int[] spells, int[] potions, long success) {
+class Solution {
