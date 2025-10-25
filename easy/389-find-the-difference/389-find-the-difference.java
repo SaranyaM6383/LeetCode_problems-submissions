1f14e7daff6413f45469800b2d62d244103a7414
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-10-25 06:22:04 +0100

    Add solution for 389. Find the Difference

---
+++ easy/389-find-the-difference/389-find-the-difference.java
@@ -0,0 +1,15 @@
+            count.put(c, count.getOrDefault(c, 0) + 1);
+        }  
+            }
+        }
+    }
+        for (char c : t.toCharArray()){
+        Map<Character, Integer> count = new HashMap<>(); 
+    public char findTheDifference(String s, String t){
+class Solution{
+                count.remove(c);
+            if(count.get(c) == 0){
+            count.put(c, count.get(c) - 1);
+        for(char c : s.toCharArray()){
+        return (char) count.keySet().toArray()[0];      
+}
