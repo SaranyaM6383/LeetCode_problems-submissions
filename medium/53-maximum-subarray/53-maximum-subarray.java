4dd6be9f73a894213fc40ed09c220d6569668e2e
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-10-27 04:03:24 +0000

    Add solution for 53. Maximum Subarray

---
+++ medium/53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,14 @@
+            res=Math.max(res,total);
+        }
+        return res;
+    }
+            total +=n;
+            }
+                total=0;
+            if(total<0){
+        for(int n:nums){
+        int total=0;
+        int res=nums[0];
+}
+    public int maxSubArray(int[] nums) {
+class Solution {
