77642a2180e5dd1cc0eb06b31b96c43c634c0c70
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-11-01 04:41:19 +0000

    Add solution for 453. Minimum Moves to Equal Array Elements

---
+++ medium/453-minimum-moves-to-equal-array-elements/453-minimum-moves-to-equal-array-elements.java
@@ -0,0 +1,13 @@
+        return count;
+        }
+            count+=n-min;
+        for(int n:nums){
+        int count=0;
+        }
+            min=Math.min(min,n);
+        for(int n:nums){
+        int min=Integer.MAX_VALUE;
+    public int minMoves(int[] nums) {
+class Solution {
+    }
+}
