4486babbd22f9a4c1b1b6a1b5febbd4aef96c2a7
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-11-01 04:31:58 +0000

    Add solution for 273. Integer to English Words

---
+++ hard/273-integer-to-english-words/273-integer-to-english-words.java
@@ -0,0 +1,13 @@
+        for (int k = i; k < s.length(); k++) {
+        }
+        if (i == s.length()) {
+            res.add(new ArrayList<>(ans));
+            return;
+    private void solve(String s, int i, List<List<String>> res, List<String> ans) {
+    }
+        }
+        return true;
+        while (i <= j) {
+            if (s.charAt(i++) != s.charAt(j--)) return false;
+class Solution {
+    private boolean isP(String s, int i, int j) {
