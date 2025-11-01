2bfa74c322f7c573cbef89a7fd8e4321d55f733c
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-11-01 04:14:52 +0000

    Add solution for 300. Longest Increasing Subsequence

---
+++ medium/300-longest-increasing-subsequence/300-longest-increasing-subsequence.java
@@ -0,0 +1,13 @@
+            }
+        }
+                int idx = binarySearch(res, n);
+                res.set(idx, n);
+                res.add(n);
+            } else {
+            if (res.isEmpty() || res.get(res.size() - 1) < n) {
+        for (int n : nums) {
+        List<Integer> res = new ArrayList<>();
+    public int lengthOfLIS(int[] nums) {
+class Solution {
+        return res.size();        
+    }
