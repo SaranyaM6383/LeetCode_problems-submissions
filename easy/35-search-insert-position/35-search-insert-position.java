3ca536c46fce91a93a0122ad5df99ef3b6500848
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-11-02 04:13:24 +0000

    Add solution for 35. Search Insert Position

---
+++ easy/35-search-insert-position/35-search-insert-position.java
@@ -0,0 +1,11 @@
+            }
+            else {
+                left=mid+1;
+            }
+        }
+        return left;
+    }
+}
+          
+            else if(nums[mid]>target){
+                right=mid-1;
