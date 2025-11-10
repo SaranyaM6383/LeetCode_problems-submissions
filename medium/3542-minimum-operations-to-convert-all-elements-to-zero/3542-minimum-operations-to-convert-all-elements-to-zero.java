e20ec540e769ff9953de8af1041e4be41cc245bf
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-11-10 13:11:41 +0000

    Add solution for 3542. Minimum Operations to Convert All Elements to Zero

---
+++ medium/3542-minimum-operations-to-convert-all-elements-to-zero/3542-minimum-operations-to-convert-all-elements-to-zero.java
@@ -0,0 +1,15 @@
+            stack.pop();
+        }
+            if(n==0)
+            continue;
+            if(stack.isEmpty() || stack.peek() <n){
+                res++;
+            }
+                stack.push(n);
+        return res;
+            while(!stack.isEmpty() && stack.peek()>n)
+        int res=0;
+        for(int n:nums){
+        Stack<Integer> stack=new Stack<>();
+class Solution {
+    public int minOperations(int[] nums) {
