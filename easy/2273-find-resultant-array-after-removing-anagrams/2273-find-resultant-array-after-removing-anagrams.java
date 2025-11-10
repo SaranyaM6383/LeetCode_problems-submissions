bfeaebdc0183dab0bb31a6db3724708f9913a836
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-11-10 13:20:21 +0000

    Add solution for 2273. Find Resultant Array After Removing Anagrams

---
+++ easy/2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -0,0 +1,13 @@
+            for (char c : w.toCharArray())
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            freq.add(map);
+        }
+        List<String> ans = new ArrayList<>();
+        ans.add(words[0]);
+        for (int i = 1; i < n; i++) {
+            if (!freq.get(i).equals(freq.get(i - 1)))
+                ans.add(words[i]);
+        }
+        return ans;
+    }
+}
