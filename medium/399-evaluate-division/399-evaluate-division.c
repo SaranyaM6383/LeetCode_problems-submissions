a76879f1524478efe6ec44877f5ede0efd785b2e
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-11-13 15:10:26 +0000

    Add solution for 399. Evaluate Division

---
+++ medium/399-evaluate-division/399-evaluate-division.c
@@ -0,0 +1,13 @@
+            if (!gr.containsKey(dividend) || !gr.containsKey(divisor)) {
+                finalAns[i] = -1.0;
+            } else {
+                HashSet<String> vis = new HashSet<>();
+                double[] ans = {-1.0};
+                double temp = 1.0;
+                dfs(dividend, divisor, gr, vis, ans, temp);
+                finalAns[i] = ans[0];
+            }
+        }
+        return finalAns;
+    }
+}
