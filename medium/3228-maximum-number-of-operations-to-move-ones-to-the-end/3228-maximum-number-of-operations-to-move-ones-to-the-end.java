24da16f019aaa57aec559294b054bd7f053d2801
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-11-13 15:08:19 +0000

    Add solution for 3228. Maximum Number of Operations to Move Ones to the End

---
+++ medium/3228-maximum-number-of-operations-to-move-ones-to-the-end/3228-maximum-number-of-operations-to-move-ones-to-the-end.java
@@ -0,0 +1,11 @@
+            if(s.charAt(i) == '1')
+            ones++;
+            else if(i>0 && s.charAt(i-1) == '1')
+       
+            res+= ones;
+        }
+        return res;
+    }
+}
+        int ones=0,res=0;
+        for(int i=0;i<s.length();i++){
