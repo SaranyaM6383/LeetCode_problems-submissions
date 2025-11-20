4d404245adac37e9d143c2f27969738c36da0dae
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-11-20 15:11:48 +0000

    Add solution for 757. Set Intersection Size At Least Two

---
+++ hard/757-set-intersection-size-at-least-two/757-set-intersection-size-at-least-two.java
@@ -0,0 +1,13 @@
+                a = r - 1;
+                b = r;
+            if (l > b) {
+            int l = it[0], r = it[1];
+        for (int[] it : intervals) {
+        int a = -1, b = -1;
+        int ans = 0;
+        );
+            a[1] == b[1] ? b[0] - a[0] : a[1] - b[1]
+        Arrays.sort(intervals, (a, b) -> 
+    public int intersectionSizeTwo(int[][] intervals) {
+class Solution {
+                ans += 2;
