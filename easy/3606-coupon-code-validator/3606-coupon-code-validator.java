ae85ca01ae39ebbcafb6af0503a088129b0d6b27
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-12-13 19:05:48 +0530

    Add solution for 3606. Coupon Code Validator

---
+++ easy/3606-coupon-code-validator/3606-coupon-code-validator.java
@@ -0,0 +1,14 @@
+            }
+        }
+        Collections.sort(e);
+        Collections.sort(g);
+        Collections.sort(p);
+        Collections.sort(r);
+        res.addAll(e);
+        res.addAll(g);
+        res.addAll(p);
+        res.addAll(r);
+        
+        return res;
+    }
+}
