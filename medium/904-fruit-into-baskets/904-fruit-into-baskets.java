090afb52dba6617c5901f6037a6fea6a75053486
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-12-24 19:42:16 +0530

    Add solution for 904. Fruit Into Baskets

---
+++ medium/904-fruit-into-baskets/904-fruit-into-baskets.java
@@ -0,0 +1,14 @@
+            map.put(fruits[end],map.getOrDefault(fruits[end],0)+1);
+            while(map.size()>=3)
+            {
+                map.put(fruits[start],map.get(fruits[start])-1);
+                if(map.get(fruits[start]) == 0) map.remove(fruits[start]);
+                start++;
+            }
+            int currLen = end-start+1;
+            maxLen = Math.max(maxLen,currLen);
+            end++;
+        }
+        return maxLen;
+    }
+}
