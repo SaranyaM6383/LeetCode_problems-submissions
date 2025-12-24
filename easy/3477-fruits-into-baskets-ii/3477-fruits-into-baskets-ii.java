fb24e6aa900fecd398d93408d5addc9b81920f60
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-12-24 19:43:09 +0530

    Add solution for 3477. Fruits Into Baskets II

---
+++ easy/3477-fruits-into-baskets-ii/3477-fruits-into-baskets-ii.java
@@ -0,0 +1,14 @@
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && baskets[j] >= fruits[i]) {
+                    used[j] = true;
+                    placed = true;
+                    break;
+                }
+            }
+            if (!placed) {
+                unplaced++;
+            }
+        }
+        return unplaced;
+    }
+}
