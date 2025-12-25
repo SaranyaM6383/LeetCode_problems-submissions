c146a003241e64368b98f4aab61120d34b742c23
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-12-25 19:21:34 +0530

    Add solution for 1262. Greatest Sum Divisible by Three

---
+++ medium/1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.java
@@ -0,0 +1,14 @@
+        
+        for (int num : nums) {
+            int[] current = dp.clone();
+            
+            for (int sum : current) {
+                int newSum = sum + num;
+                int remainder = newSum % 3;
+                dp[remainder] = Math.max(dp[remainder], newSum);
+            }
+        }
+        
+        return dp[0];
+    }
+}
