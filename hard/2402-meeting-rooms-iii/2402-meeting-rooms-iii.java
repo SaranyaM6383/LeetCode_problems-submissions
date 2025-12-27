d7368ca370b6ef4845b148cfbd0f35acdae7b27a
SaranyaM6383 <saranyakowsalya97@gmail.com>
2025-12-27 20:05:31 +0530

    Add solution for 2402. Meeting Rooms III

---
+++ hard/2402-meeting-rooms-iii/2402-meeting-rooms-iii.java
@@ -0,0 +1,12 @@
+            }
+        }
+        int maxMeetingCount = 0, maxMeetingCountRoom = 0;
+        for (int i = 0; i < n; i++) {
+            if (meetingCount[i] > maxMeetingCount) {
+                maxMeetingCount = meetingCount[i];
+                maxMeetingCountRoom = i;
+            }
+        }
+        return maxMeetingCountRoom;
+    }
+}
