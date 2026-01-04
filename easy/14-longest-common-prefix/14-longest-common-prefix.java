45fcc1dd8a016f917bf9e651dae4a8b9bd1b648f
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-01-04 10:32:10 +0530

    Add solution for 14. Longest Common Prefix

---
+++ easy/14-longest-common-prefix/14-longest-common-prefix.java
@@ -0,0 +1,8 @@
+            if (first.charAt(i) != last.charAt(i)) {
+                return ans.toString();
+            }
+            ans.append(first.charAt(i));
+        }
+        return ans.toString();
+    }
+}
