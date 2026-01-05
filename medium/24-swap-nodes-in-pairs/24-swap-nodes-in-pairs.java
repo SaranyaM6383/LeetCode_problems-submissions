f6239e65912bc4290783ffb7167408b76f2265a2
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-01-05 20:19:48 +0530

    Add solution for 24. Swap Nodes in Pairs

---
+++ medium/24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.java
@@ -0,0 +1,9 @@
+            }
+                i++;
+                nums[i] = nums[j];
+            if(nums[j] !=nums[i-1]){
+        for(int j=1;j<nums.length;j++){
+        int i=1;
+        if(nums.length==0) return 0;
+    public int removeDuplicates(int[] nums) {
+class Solution {
