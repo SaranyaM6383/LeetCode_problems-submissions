08d8cd862f0ee13b3096744fd19f65dee7765d5a
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-01-05 20:21:05 +0530

    Add solution for 27. Remove Element

---
+++ easy/27-remove-element/27-remove-element.java
@@ -0,0 +1,9 @@
+        for(int i=0;i<nums.length;i++){
+            if(nums[i] !=val){
+                nums[k]=nums[i];
+            }
+        }
+                k++;
+    }
+        return k;
+}
