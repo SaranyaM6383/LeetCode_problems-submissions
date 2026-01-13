60be7e911274d1c7bfc6c88756f9e73348c6fca6
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-01-13 20:57:15 +0530

    Add solution for 3453. Separate Squares I

---
+++ medium/3453-separate-squares-i/3453-separate-squares-i.java
@@ -0,0 +1,8 @@
+                // We take the width (l) * the height of the bottom portion 
+(currentY - y)
+                area += l * (currentY - y);
+            }
+        }
+        return area;
+    }
+}
