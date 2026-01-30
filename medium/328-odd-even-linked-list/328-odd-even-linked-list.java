c4fd6d7b4caaadce627f2a11b3a6dc64aa57f32f
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-01-30 12:48:37 +0530

    Add solution for 328. Odd Even Linked List

---
+++ medium/328-odd-even-linked-list/328-odd-even-linked-list.java
@@ -0,0 +1,10 @@
+        return oddStart;
+    }
+}
+        evenEnd.next=null;
+        oddEnd.next=evenStart;
+        if(evenStart == null) return oddStart;
+        if(oddStart == null) return evenStart;
+        }
+                pos++;
+                temp=temp.next;
