d4b8bcdc68d25edd88f2aa4c59c65e0a2bcd90bc
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-02-01 08:04:06 +0530

    Add solution for 3010. Divide an Array Into Subarrays With Minimum Cost I

---
+++ easy/3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -0,0 +1,8 @@
+                a=A[i];
+            }else if(A[i]<b)
+        int a=51,b=51;
+        for(int i=1;i<A.length;i++){
+            if(A[i]<a){
+                b=a;
+class Solution {
+    public int minimumCost(int[] A) {
