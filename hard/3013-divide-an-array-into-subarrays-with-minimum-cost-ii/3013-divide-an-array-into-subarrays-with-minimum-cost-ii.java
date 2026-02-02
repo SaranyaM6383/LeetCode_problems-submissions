0478c16439566dc82b3a62a0775ef0e224f0efca
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-02-02 17:53:23 +0530

    Add solution for 3013. Divide an Array Into Subarrays With Minimum Cost II

---
+++ hard/3013-divide-an-array-into-subarrays-with-minimum-cost-ii/3013-divide-an-array-into-subarrays-with-minimum-cost-ii.java
@@ -0,0 +1,8 @@
+        for(int i = 2; i + dist < n; i ++){
+            window.remove(nums[i - 1]);
+            window.add(nums[i + dist]);
+            ans = Math.min(ans, window.query());
+        }
+        return ans + nums[0];
+    }
+}
