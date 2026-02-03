93808dde8ae78d569664d7d104ee5d3c7a79733c
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-02-03 22:27:31 +0530

    Add solution for 3637. Trionic Array I

---
+++ easy/3637-trionic-array-i/3637-trionic-array-i.java
@@ -0,0 +1,8 @@
+        int q = i - 1;
+        while (i < n && nums[i - 1] < nums[i]) {
+            i++;
+        }
+        int flag = i - 1;
+        return (p != 0) && (q != p) && (flag == n - 1 && flag != q);
+    }
+}
