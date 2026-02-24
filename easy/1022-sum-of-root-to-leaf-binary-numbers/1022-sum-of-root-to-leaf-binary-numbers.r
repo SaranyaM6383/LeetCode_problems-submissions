ff22871ba730a1725546ad5c0e912557f3a9c63b
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-02-24 22:18:15 +0530

    Add solution for 1022. Sum of Root To Leaf Binary Numbers

---
+++ easy/1022-sum-of-root-to-leaf-binary-numbers/1022-sum-of-root-to-leaf-binary-numbers.r
@@ -0,0 +1,10 @@
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root,0);
+    }
+    private int dfs(TreeNode node,int current){
