a9dfe81062daf224ee80f646a081652f04258dc7
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-03-03 21:00:07 +0530

    Add solution for 655. Print Binary Tree

---
+++ medium/655-print-binary-tree/655-print-binary-tree.java
@@ -0,0 +1,11 @@
+        printTree(root.left,row+1,left,col-1);
+        res.get(row).set(col,Integer.toString(root.val));
+        int col=(left+right)/2;
+        if(root==null)return;
+    private void printTree(TreeNode root,int row,int left,int right){
+    }
+        return res;
+        printTree(root,0,0,cols-1);
+        }
+            }
+                res.get(i).add("");
