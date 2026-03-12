bb45aa62c767f51a1bf9e5a705f8c96a545db0b1
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-03-12 21:29:48 +0530

    Add solution for 347. Top K Frequent Elements

---
+++ medium/347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -0,0 +1,10 @@
+        pq.offer(num);
+     for(int num:map.keySet()){
+     PriorityQueue<Integer> pq=new PriorityQueue<>((a,b) -> map.get(a)-map.get(b));
+     }   
+        map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+     for(int i=0;i<n;i++){
+     HashMap<Integer,Integer> map=new HashMap<>();
+     int n=nums.length;
+    public int[] topKFrequent(int[] nums, int k) {
+class Solution {
