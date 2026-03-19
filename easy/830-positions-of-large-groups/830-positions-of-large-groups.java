2c34a924f5d0349d6ef80f8605ea1350254d03d7
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-03-19 09:01:59 +0530

    Add solution for 830. Positions of Large Groups

---
+++ easy/830-positions-of-large-groups/830-positions-of-large-groups.java
@@ -0,0 +1,10 @@
+        if(j==N-1 || S.charAt(j)!=S.charAt(j+1)){
+            if(j-i+1 >=3)
+        }
+            ans.add(Arrays.asList(new Integer[]{i,j}));
+            i=j+1;
+       for(int j=0;j<N;++j){
+       int i=0,N=S.length();
+       List<List<Integer>> ans=new ArrayList();
+    public List<List<Integer>> largeGroupPositions(String S) {
+class Solution {
