d99fb8c81f113f0538d41f08f34948d8275e1967
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-03-29 20:11:23 +0530

    Add solution for 2839. Check if Strings Can be Made Equal With Operations I

---
+++ easy/2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.java
@@ -0,0 +1,6 @@
+(3) == s2.charAt(3)) ||
+                                                       (s1.charAt(1) == s2.charAt
+(3) && s1.charAt(3) == s2.charAt(1)));
+                                                           
+    }
+}
