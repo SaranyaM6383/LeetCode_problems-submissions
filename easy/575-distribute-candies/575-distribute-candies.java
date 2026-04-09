c3f2fd6556de8a4cff95f82b61baab280119af71
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-04-09 16:20:52 +0530

    Add solution for 575. Distribute Candies

---
+++ easy/575-distribute-candies/575-distribute-candies.java
@@ -0,0 +1,10 @@
+        set.add(candy);
+      }  
+      int MaxEat=candyType.length/2;
+    }
+      return Math.min(set.size(),MaxEat);
+      for(int candy:candyType){
+      HashSet<Integer> set=new HashSet<>();
+    public int distributeCandies(int[] candyType) {
+class Solution {
+import java.util.HashSet;
