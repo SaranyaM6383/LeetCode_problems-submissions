166ce74488afd192eb2c2373bb08e86b725f1bbb
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-04-28 15:21:02 +0100

    Add solution for 2033. Minimum Operations to Make a Uni-Value Grid

---
+++ medium/2033-minimum-operations-to-make-a-uni-value-grid/2033-minimum-operations-to-make-a-uni-value-grid.java
@@ -0,0 +1,10 @@
+              for (int i = min; i <= max; i += x)
+                                                                                      
+                                                                                      
+                                                                                      
+                          ops += Math.abs(i - median) / x * freq[i];
+                                                                                      
+                                                                                      
+                                                                                      
+                                  return ops; 
+    }
