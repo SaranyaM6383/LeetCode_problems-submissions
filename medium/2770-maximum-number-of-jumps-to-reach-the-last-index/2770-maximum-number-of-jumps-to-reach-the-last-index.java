b2ff8288130ecc40c84fd8550548e22001c56851
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-05-10 04:01:18 +0100

    Add solution for 2770. Maximum Number of Jumps to Reach the Last Index

---
+++ medium/2770-maximum-number-of-jumps-to-reach-the-last-index/2770-maximum-number-of-jumps-to-reach-the-last-index.java
@@ -0,0 +1,7 @@
+-1) continue;
+                                                                        if(dp[i] == 
+                                                            // unreachable index
+                                                for(int i = 0; i < n; i++) {
+                                        dp[0] = 0;
+                                // base case
+                        Arrays.fill(dp, -1);
