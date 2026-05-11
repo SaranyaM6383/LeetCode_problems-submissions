1fe01ed1ff085a63b8244e541d82272e6e9d1007
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-05-11 16:23:59 +0100

    Add solution for 2553. Separate the Digits in an Array

---
+++ easy/2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.java
@@ -0,0 +1,11 @@
+        int[] result=new int[list.size()];
+        for(int i=0;i<list.size();i++){
+            }
+        }
+                list.add(ch - '0');
+            for(char ch:s.toCharArray()){
+            String s=String.valueOf(num);
+        List<Integer> list=new ArrayList<>();
+        for(int num:nums){
+    public int[] separateDigits(int[] nums) {
+class Solution {
