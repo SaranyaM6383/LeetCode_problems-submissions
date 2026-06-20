e9d474ba08960cfb87151a50224ec11118a1eee9
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-06-20 08:03:32 +0100

    Add solution for 3871. Count Commas in Range II

---
+++ medium/3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long countCommas(long n) {
+        long ans=0;
+        long p=1000;
+        for(int i=1;i<=5;i++){
+            if(n>=p){
+        }
+        return ans;
+                ans+=n-p+1;
+            }
+            p*=1000;
