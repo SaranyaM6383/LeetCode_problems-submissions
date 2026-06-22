7c274fffd91ba5ea4e789d7764e97f62767aa8ae
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-06-22 10:08:19 +0100

    Add solution for 3829. Design Ride Sharing System

---
+++ medium/3829-design-ride-sharing-system/3829-design-ride-sharing-system.java
@@ -0,0 +1,14 @@
+        driver.add(driverId);
+    
+    public void addDriver(int driverId) {
+    }
+        rider.add(riderId);
+        
+        if(cancel.containsKey(riderId)) cancel.remove(riderId);
+    public void addRider(int riderId) {
+    
+    public RideSharingSystem() {}
+    java.util.Map<Integer,Integer> cancel=new java.util.HashMap<>();
+    java.util.Queue<Integer> rider=new java.util.LinkedList<>();
+    java.util.Queue<Integer> driver=new java.util.LinkedList<>();
+class RideSharingSystem {
