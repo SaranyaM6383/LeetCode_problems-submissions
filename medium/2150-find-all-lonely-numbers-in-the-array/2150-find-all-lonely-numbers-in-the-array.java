a2a2ce63139eae49ba02dc0bce3ac4cfd46d4de6
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-06-29 09:50:32 +0100

    Add solution for 2150. Find All Lonely Numbers in the Array

---
+++ medium/2150-find-all-lonely-numbers-in-the-array/2150-find-all-lonely-numbers-in-the-array.java
@@ -0,0 +1,14 @@
+                }
+                if(map.get(num)==1 && !map.containsKey(num-1) && !map.containsKey
+                (num+1)){
+                    list.add(num);
+            for(int num:nums){
+            }
+            List<Integer> list=new ArrayList<>();
+    public List<Integer> findLonely(int[] nums) {
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        HashMap<Integer,Integer> map=new HashMap<>();
+            }
+            return list;
+    }
