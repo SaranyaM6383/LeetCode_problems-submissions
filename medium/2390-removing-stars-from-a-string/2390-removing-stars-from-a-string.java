8a6634990be871d735a01ecef4539320eefd16a8
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-07-01 06:28:40 +0100

    Add solution for 2390. Removing Stars From a String

---
+++ medium/2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.java
@@ -0,0 +1,13 @@
+    public String removeStars(String s) {
+        StringBuilder c=new StringBuilder();
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='*'){
+                c.deleteCharAt(c.length()-1);
+            } else{
+        }
+                c.append(s.charAt(i));
+            }
+    }
+        return c.toString();
+}
+class Solution {
