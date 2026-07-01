818cf5a72e376fad4cfdf8b9b7337efadc4de22f
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-07-01 10:35:16 +0100

    Add solution for 2270. Number of Ways to Split Array

---
+++ medium/2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.java
@@ -0,0 +1,11 @@
+           right-=nums[i];
+           left+=nums[i];
+        for(int i=0;i<nums.length-1;i++){
+        int ans=0;
+        }
+           if(left>=right){
+            ans++;
+           
+            }
+        }
+        return ans;
