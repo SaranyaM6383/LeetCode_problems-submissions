7333dcf81f3b68eac8db8adc240b1044ddf8b5f8
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-07-02 09:27:18 +0100

    Add solution for 2134. Minimum Swaps to Group All 1's Together II

---
+++ medium/2134-minimum-swaps-to-group-all-1s-together-ii/2134-minimum-swaps-to-group-all-1s-together-ii.java
@@ -0,0 +1,10 @@
+        int maxOnes=curr;
+        }
+            curr+=nums[i];
+        for(int i=0;i<ones;i++){
+        int curr=0;
+        if(ones==0 || ones==n) return 0;
+        }
+           ones+=num;
+        for(int num:nums){
+        for(int i=ones;i<n+ones;i++){
