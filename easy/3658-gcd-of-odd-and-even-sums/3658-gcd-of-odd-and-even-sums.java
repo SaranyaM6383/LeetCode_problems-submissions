8ce406fdd0347d3a280f49a601234a78135d7e3f
SaranyaM6383 <saranyakowsalya97@gmail.com>
2026-07-15 09:49:18 +0100

    Add solution for 3658. GCD of Odd and Even Sums

---
+++ easy/3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int gcdOfOddEvenSums(int n) {
+        return gcd(n*(n+1),n*n);
+    }
+    public int gcd(int a,int b){
+        if(b==0) return a;
+        else return gcd(b,(a%b));
+    }
+}
+    
